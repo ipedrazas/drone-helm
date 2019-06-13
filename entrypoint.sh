ffb57969be91bf3efa00b7de4686bd97c5da4ec0
Paul Rogalinski-Pinter <paul@paul.vc>
2019-06-13 22:32:45 +0200

    fixed wrong env var case

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ HELM_VERSION="${HELM_VERSION:-v2.14.1}"
 KUBECTL_VERSION="${KUBECTL_VERSION:-v1.14.3}"
 
 mkdir -p ${PREFIX}/bin
-ln -s -f ${PREFIX}/lib/helm-${HELM_VERSION}/helm ${prefix}/bin
-ln -s -f ${PREFIX}/lib/kubectl-${KUBECTL_VERSION}/kubectl ${prefix}/bin
+ln -s -f ${PREFIX}/lib/helm-${HELM_VERSION}/helm ${PREFIX}/bin
+ln -s -f ${PREFIX}/lib/kubectl-${KUBECTL_VERSION}/kubectl ${PREFIX}/bin
 
 export PATH=${PREFIX}/bin:$PATH
 
